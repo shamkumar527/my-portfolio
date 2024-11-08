6790599fefe3ca087da585570161794c98c125ec
shamkumar527 <131437819+shamkumar527@users.noreply.github.com>
2024-11-08 12:16:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    if (name && email && message) {
+      document.getElementById("responseMessage").textContent = "Thank you for your message!";
+    } else {
+      document.getElementById("responseMessage").textContent = "Please fill in all fields.";
+    }
+  });
+  
